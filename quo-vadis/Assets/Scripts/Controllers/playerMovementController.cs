328e4a628ce5fd36c43cfd155b9f97782646b5cd
Josh Arabia <jra65@pitt.edu>
2019-02-24 19:22:55 -0500

    Added playerMovementController and playerManager

---
+++ quo-vadis/Assets/Scripts/Controllers/playerMovementController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementController : MonoBehaviour
+{
+
+    private float maxSpeed;
+    private float acceleration;
+
+    public PlayerManager playerManager;
+    private Rigidbody2D rb;
+
+    private float xSpeed = 0;
+    private float ySpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxSpeed = playerManager.maxSpeed;
+        acceleration = playerManager.acceleration;
+        rb = playerManager.rb;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        rb.velocity = new Vector2(xSpeed, ySpeed);
+    }
+
+    private void move()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            xSpeed = Mathf.Clamp(xSpeed + acceleration, -maxSpeed, maxSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            xSpeed = Mathf.Clamp(xSpeed - acceleration, -maxSpeed, maxSpeed);
+        }
+
+        if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            if(xSpeed >= 0f)
+            {
+                xSpeed = Mathf.Clamp(xSpeed - acceleration, 0f, maxSpeed);
+            }
+            else
+            {
+                xSpeed = Mathf.Clamp(xSpeed + acceleration, -maxSpeed, 0f);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            ySpeed = Mathf.Clamp(ySpeed + acceleration, -maxSpeed, maxSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            ySpeed = Mathf.Clamp(ySpeed - acceleration, -maxSpeed, maxSpeed);
+        }
+
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            if (ySpeed >= 0f)
+            {
+                ySpeed = Mathf.Clamp(ySpeed - acceleration, 0f, maxSpeed);
+            }
+            else
+            {
+                ySpeed = Mathf.Clamp(ySpeed + acceleration, -maxSpeed, 0f);
+            }
+        }
+    }
+}
---
+++ quo-vadis/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public float maxSpeed;
+    public float acceleration;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer sr;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
