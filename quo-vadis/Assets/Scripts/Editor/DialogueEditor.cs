47a1257b777ca8f5cb8ede0924fe8ff7cccc41e8
Josh Arabia <jra65@pitt.edu>
2019-02-25 22:57:05 -0500

    Created dialogue storage system, Added dialogue editor, Created interactable object framework

---
+++ quo-vadis/Assets/Scripts/Editor/DialogueEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class DialogueEditor : EditorWindow
+{
+
+    public DialogueData dialogue;
+    private string relativeFilePath = null;
+
+    [MenuItem ("Window/Dialogue Editor")]
+    static void Init()
+    {
+        DialogueEditor window = (DialogueEditor)EditorWindow.GetWindow(typeof(DialogueEditor));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        relativeFilePath = EditorGUILayout.TextField("FILE NAME", relativeFilePath);
+        if (dialogue != null)
+        {
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty serializedProperty = serializedObject.FindProperty("dialogue");
+
+            EditorGUILayout.PropertyField(serializedProperty, true);
+
+            serializedObject.ApplyModifiedProperties();
+
+            if (GUILayout.Button("Save")) 
+            {
+                SaveDialogueData();
+            }
+        }
+
+        if (GUILayout.Button("Load"))
+        {
+            LoadDialogueData(relativeFilePath);
+        }
+
+    }
+
+    private void LoadDialogueData(string dialogueFilePath)
+    {
+        relativeFilePath = dialogueFilePath;
+        string absoluteFilePath = Path.Combine(Application.streamingAssetsPath, dialogueFilePath);
+
+        if (File.Exists(absoluteFilePath))
+        {
+            string dataAsJson = File.ReadAllText(absoluteFilePath);
+            dialogue = JsonUtility.FromJson<DialogueData>(dataAsJson);
+
+        }
+        else
+        {
+            Debug.Log("Generated new Dialogue Data at:" + absoluteFilePath);
+            dialogue = new DialogueData();
+        }
+    }
+
+    private void SaveDialogueData()
+    {
+        string dataAsJson = JsonUtility.ToJson(dialogue);
+
+        string absoluteFilePath = Path.Combine(Application.streamingAssetsPath, relativeFilePath);
+
+        File.WriteAllText(absoluteFilePath, dataAsJson);
+    }
+
+}
